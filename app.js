cea133284604c6f6dd3b2d3af89450ff86fc11b5
MEGA-BLASTER <devmegablaster@gmail.com>
2021-09-22 18:58:20 +0530

    Added changes in weather location output

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ request({url: weather_URL, json: true}, (error, response) => {
     }else if(response.body.current === undefined){
         console.log(chalk.red.bold("Unable to find"), chalk.whiteBright.bgRed.bold(location))
     }else {
-        console.log(chalk.blue("Current weather in"), chalk.green.bold.underline(location), "is", chalk.blueBright.bold(response.body.current.weather_descriptions[0]), chalk.blue("and the current temperature is"), chalk.blueBright.bold(response.body.current.temperature) + chalk.blueBright("°C"))
+        console.log(chalk.blue("Current weather in"), chalk.green.bold.underline(response.body.location.name + "," + response.body.location.country), "is", chalk.blueBright.bold(response.body.current.weather_descriptions[0]), chalk.blue("and the current temperature is"), chalk.blueBright.bold(response.body.current.temperature) + chalk.blueBright("°C"))
     }
 })
